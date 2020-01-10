428b58fb519bef2b394b02177611e79e9eddbcc9
Juan Sebastian Calle <jusemonca@gmail.com>
2020-01-10 15:03:29 -0500

    [Feature]: script that pack files folder web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Script that generates a tgz arvhive from the contents of the web_static
+from fabric.api import local
+import datetime
+
+
+def do_pack():
+    """Function that pack all files of the web_static
+    """
+    now = datetime.datetime.now()
+    local("mkdir -p versions/")
+    file = "versions/web_static_{}{}{}{}.tgz".format(now.year,
+                                                      now.month,
+                                                      now.minute,
+                                                      now.second)
+    local("sudo tar -cvzf {} web_static".format(file))
+    if file:
+        return file
+    return None
