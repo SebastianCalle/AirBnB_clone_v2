893ea3ae82688f8ece581f5841a1787e8ddc925d
Juan Sebastian Calle <jusemonca@gmail.com>
2020-01-10 20:57:47 -0500

    [Feature]: Script that distributes archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,24 +10,23 @@ env.user = "ubuntu"
 def do_deploy(archive_path):
     """Function that deploy using fabric
     """
-    if os.path.isfile(archive_path):
-        try:
-            file = archive_path.split("/")[1]
-            directory = file.split(".")[0]
-            put("{:s}".format(archive_path), "/tmp/")
-            run("sudo mkdir -p /data/web_static/releases/{:s}".format(directory))
-            run("sudo tar -xzf /tmp/{:s} -C\
-                /data/web_static/releases/{:s}".format(file, directory))
-            run("sudo rm /tmp/{:s}".format(file))
-            run("sudo mv /data/web_static/releases/{:s}/web_static/*\
-                /data/web_static/releases/{:s}".format(directory, directory))
-            run("sudo rm -rf /data/web_static/releases/{:s}\
-                /web_static".format(directory))
-            run("sudo rm -rf /data/web_static/current")
-            run("sudo ln -s /data/web_static/releases/{:s}\
-                /data/web_static/current".format(directory))
-            return True
-        except:
+    try:
+        if os.path.exists(archive_path):
             return False
-    else:
+        file = archive_path.split("/")[1]
+        directory = file.split(".")[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{:s}/".format(directory))
+        run("tar -xzf /tmp/{:s} -C /data/web_static/releases/{:s}/"
+            .format(file, directory))
+        run("rm /tmp/{:s}".format(file))
+        run("mv /data/web_static/releases/{:s}/web_static/* \
+            /data/web_static/releases/{:s}/".format(directory, directory))
+        run("rm -rf /data/web_static/releases/{:s}/web_static"
+            .format(directory))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{:s} /data/web_static/current"
+            .format(directory))
+        return True
+    except:
         return False
