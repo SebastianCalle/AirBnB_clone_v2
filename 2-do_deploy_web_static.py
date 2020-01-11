9ee9405ebbff56b7e9b8ecebbd2bd7ced51f7ab8
Juan Sebastian Calle <jusemonca@gmail.com>
2020-01-10 20:19:52 -0500

    [Feature]: Script that distributes archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Script that distributes an archive to your web servers
 from fabric.api import *
-from os.path import exists
+import os.path
 
 env.hosts = ['34.73.226.110', '35.243.175.109']
 env.user = "ubuntu"
@@ -10,22 +10,24 @@ env.user = "ubuntu"
 def do_deploy(archive_path):
     """Function that deploy using fabric
     """
-    if exists(archive_path):
-        file = archive_path.split("/")[1]
-        directory = file.split(".")[0]
-        print(directory)
-        put("{}".format(archive_path), "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/{}".format(directory))
-        run("sudo tar -xzf /tmp/{} -C\
-            /data/web_static/releases/{}".format(file, directory))
-        run("rm /tmp/{}".format(file))
-        run("sudo mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}".format(directory, directory))
-        run("sudo rm -rf /data/web_static/releases/{}\
-            /web_static".format(directory))
-        run("rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}\
-            /data/web_static/current".format(directory))
-        return True
+    if os.path.isfile(archive_path):
+        try:
+            file = archive_path.split("/")[1]
+            directory = file.split(".")[0]
+            put("{:s}".format(archive_path), "/tmp/")
+            run("sudo mkdir -p /data/web_static/releases/{:s}".format(directory))
+            run("sudo tar -xzf /tmp/{:s} -C\
+                /data/web_static/releases/{:s}".format(file, directory))
+            run("sudo rm /tmp/{:s}".format(file))
+            run("sudo mv /data/web_static/releases/{:s}/web_static/*\
+                /data/web_static/releases/{:s}".format(directory, directory))
+            run("sudo rm -rf /data/web_static/releases/{:s}\
+                /web_static".format(directory))
+            run("sudo rm -rf /data/web_static/current")
+            run("sudo ln -s /data/web_static/releases/{:s}\
+                /data/web_static/current".format(directory))
+            return True
+        except:
+            return False
     else:
         return False
