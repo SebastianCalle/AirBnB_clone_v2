243f343bbd77169a5c562a508629fbf8155b0d00
Juan Sebastian Calle <jusemonca@gmail.com>
2020-01-20 15:34:14 -0500

    [Feature]: Add path number only if is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# Script that starts a flask web application
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Function that display Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Function that display HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def cprograming(text):
+    """
+    Function that display C
+    """
+    string = text.replace('_', ' ')
+    return 'C {}'.format(string)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyhthon_programming(text="is cool"):
+    """
+    Function that display Python
+    """
+    string = text.replace('_', ' ')
+    return 'Python {}'.format(string)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Function that display number
+    """
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
