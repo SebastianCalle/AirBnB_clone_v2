22e5b515bb40bbc77274b79c9e8ddda61da5d1e8
Juan Sebastian Calle <jusemonca@gmail.com>
2020-01-20 12:47:39 -0500

    [Feature]: Script that start a flask app and display rout /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Script that starts a flask web application
+
+from flask import Flask
+
+app = Flask(__name__)
+app.run(host='0.0.0.0', port=5000)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Function that display Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """
+    Function that display HBNB
+    """
+    return 'HBNB'
